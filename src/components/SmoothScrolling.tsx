5ca68fdf0049a38a859ff5b49ca45d8a636a80ba
[Aaditya1273] <aadityaiitian1515@gmail.com>
2025-12-15 14:18:05 +0530

    Arbitrum

--- src/components/SmoothScrolling.tsx
+++ src/components/SmoothScrolling.tsx
@@ -5,16 +5,7 @@ import Lenis from 'lenis';
 
 export default function SmoothScrolling() {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
-    });
+    const lenis = new Lenis();
 
     function raf(time: number) {
       lenis.raf(time);
